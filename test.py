ecf6336c0dd25a5f70b19fccb2721eb6bce825d0
rexnz <michaeltropotov@yahoo.com>
2024-05-08 09:36:55 +1200

    test commit

--- test.py
+++ test.py
@@ -1,8 +1,28 @@
-print ("test")
-var1 = 10
-if var1>20 :
-    print ("aa")
-else   :
-    print ("bb")
+type b = bool;
+print (b)
 
-print ("223")
+
+# pi=3.14
+# print(3=4)
+# print(type(pi))
+
+
+# name = "String"
+# name2 = str(name)
+# age = int (44)
+# print ("name=", name, " name2=", name2, " age=",age)
+
+
+
+print ("--end of run--")
+
+# var1 = 10
+# if var1>20 :
+#     print ("aa")
+# else   :
+#     print ("bb")
+
+# print ("223")
+
+
+
