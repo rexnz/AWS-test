855b46db1e0f77a46de41e50e69a95d6e50fa2c1
rexnz <michaeltropotov@yahoo.com>
2024-05-08 11:44:51 +1200

    Revert "test commit"
    This reverts commit ecf6336c0dd25a5f70b19fccb2721eb6bce825d0.

--- test.py
+++ test.py
@@ -1,29 +1,8 @@
-type b = bool;
-print (b)
+print ("test")
+var1 = 10
+if var1>20 :
+    print ("aa")
+else   :
+    print ("bb")
 
-
-# pi=3.14
-# print(3=4)
-# print(type(pi))
-
-
-# name = "String"
-# name2 = str(name)
-# age = int (44)
-# print ("name=", name, " name2=", name2, " age=",age)
-
-
-
-print ("--end of run--")
-
-# var1 = 10
-# if var1>20 :
-#     print ("aa")
-# else   :
-#     print ("bb")
-
-# print ("223")
-
-
-
-# second line
+print ("223")
