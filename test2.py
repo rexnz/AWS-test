d62a9e68cc9763676e937d2e30d996b65fabe0c1
rexnz <michaeltropotov@yahoo.com>
2024-05-09 22:59:52 +1200

    test2

--- test2.py
+++ test2.py
@@ -1,5 +1,19 @@
-greeting = "Hello"
-name = input("Please enter name:")
+# greeting = "Hello"
+# name = input("Please enter name:")
 
 #estra line
+
+
+
+greeting = "Hello"
+name = "Tim"
+print (greeting + ' ' + name)
+age = int(24)
+print (age)
+
+print (type(age))
+print (type(name))
+
+print (name + age)
+
 
